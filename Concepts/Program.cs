5f13a713d344ed1a1f12ab93842e958b4a75275b
shakerkamal <shakerkamal18@gmail.com>
2024-04-19 18:30:04 +0200

    Complex operators

---
+++ Concepts/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Concepts
+{
+
+    public struct Complex
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Complex a = new Complex() { Real = 3, Imaginary = 5 };
+            Complex b = new Complex() { Real = 1, Imaginary = 3 };
+            Complex c = a + b;
+            //sbyte b = 127;
+            Console.WriteLine("Hello World!");
+        }
+
+        //Overloadable operators  --------> +, -, /, *, %, &, |, ++, --, true, false, comparison operators(>, <, ==, !=) must be overloaded in pairs  
+        public static Complex operator +(Complex a, Complex b){
+            return new Complex{
+                Real = a.Real + b.Real,
+                Imaginary = a.Imaginary + b.Imaginary
+            };
+        }
+        
+    }
+}
+
+
