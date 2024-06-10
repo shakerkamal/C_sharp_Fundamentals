f77627d974b62e8ff6582b31bc8b22e27a4c2d99
shakerkamal <shakerkamal18@gmail.com>
2024-06-10 22:31:11 +0200

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i =0; i< 10; i++)
+            //{
+            //    Console.WriteLine("Hello World! {0}",i );
+            //}
+            int counter = 0;
+            //do
+            //{
+            //    Console.WriteLine(counter);
+            //    counter++;
+            //} while (counter < 10);
+
+            while(counter < 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            Console.Read();
+        }
+    }
+}
