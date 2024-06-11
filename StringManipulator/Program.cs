6a6b7f36e9ed9f76116c03e9b21d44bc985c9e67
shakerkamal <shakerkamal18@gmail.com>
2024-06-11 19:43:13 +0200

    String manipulation

---
+++ StringManipulator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Test123-1";
+            string t = "Test123";
+            string[] vals = s.Split("-");
+            string[] ts = t.Split("-");
+
+            foreach(var v in vals){
+                System.Console.WriteLine(v);
+            }
+
+            foreach(var r in ts){
+                System.Console.WriteLine(r);
+            }
+         }
+    }
+}
